afaa187b712c93524ec02fd7c1579e40e3c55e1b
RohanGautam <rohan1gautam@gmail.com>
2020-04-08 18:08:52 +0800

    added firebase notif send code (i think)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,19 +2,30 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
+const fcm = admin.messaging();
+
 exports.notifyUser = functions.https.onCall(
     (data, context)=>{
         const users = admin.firestore().collection('userData'); //just a reference
         console.log("notifyUser called!")
         users.get().then(function(querySnapshot) {
-           querySnapshot.forEach(function(doc){
+           querySnapshot.forEach(async function (doc){
             console.log(doc.id, " => ", doc.data());
+            const notificationPayload: admin.messaging.MessagingPayload = {
+                notification : {
+                    title: 'Alert!',
+                    body: `You are ${doc.id}. Thanks for your contribution!`,
+                    clickAction: 'FLUTTER_NOTIFICATION'
+                }
+            }
+            const result = await fcm.sendToDevice(doc.id, notificationPayload);
+            console.log(`Send notification result : ${result}`)
            })
         }).catch(function(error) {
             console.log("Error getting document:", error);
         });
         return {
-            'users': users
+            'users': "not yet"
         };
     }
 );
